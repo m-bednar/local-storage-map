bb6756e5790fc702d8d17e3f0e4ec8c219ca7faa
Martin Bednář <mbedna8@seznam.cz>
2024-07-15 15:26:59 +0200

    implement rest of the tests

--- test/LocalStorageMap.spec.ts
+++ test/LocalStorageMap.spec.ts
@@ -66,7 +66,7 @@ describe('LocalStorageMap', () => {
 
     describe('when asked to delete key', () => {
         describe('and key does exists', () => {
-            it('should return true and save object without the key', () => {
+            it('should return true and save object without the deleted key', () => {
                 const { masterKey, storage, localStorageMap } = setup({ key: 'value', anotherKey: 'another-value' });
                 const result = localStorageMap.delete('key');
 
@@ -84,6 +84,80 @@ describe('LocalStorageMap', () => {
                 expect(storage.setItem).to.not.be.called;
             });
         });
+    });
+
+    describe('when called with forEach', () => {
+        it('should call callback on every stored key-value', () => {
+            const data: Record<string, any> = { key1: 'value1', key2: 'value2', key3: 'value3' };
+            const { localStorageMap } = setup(data);
+
+            localStorageMap.forEach((value, key, map) => {
+                expect(key in data).to.be.true;
+                expect(value).to.be.equal(data[key]);
+                expect(map).to.be.equal(localStorageMap);
+            });
+        }); 
+    });
+
+    describe('when asked to get entries', () => {
+        it('should return all entries', () => {
+            const data: Record<string, any> = { key1: 'value1', key2: 'value2', key3: 'value3' };
+            const { localStorageMap } = setup(data);
+
+            const entries = Array.from(localStorageMap.entries());
+            expect(entries).to.be.deep.equal([['key1', 'value1'], ['key2', 'value2'], ['key3', 'value3']]);
+        }); 
+    });
+
+    describe('when asked to get keys', () => {
+        it('should return all keys', () => {
+            const data: Record<string, any> = { key1: 'value1', key2: 'value2', key3: 'value3' };
+            const { localStorageMap } = setup(data);
+
+            const entries = Array.from(localStorageMap.keys());
+            expect(entries).to.be.deep.equal(['key1', 'key2', 'key3']);
+        }); 
+    });
+
+    describe('when asked to get values', () => {
+        it('should return all values', () => {
+            const data: Record<string, any> = { key1: 'value1', key2: 'value2', key3: 'value3' };
+            const { localStorageMap } = setup(data);
+
+            const entries = Array.from(localStorageMap.values());
+            expect(entries).to.be.deep.equal(['value1', 'value2', 'value3']);
+        }); 
+    });
+
+    describe('when being iterated', () => {
+        it('should iterate trough all entries', () => {
+            const data: Record<string, any> = { key1: 'value1', key2: 'value2', key3: 'value3' };
+            const { localStorageMap } = setup(data);
+
+            for (const [key, value] of localStorageMap) {
+                expect(key in data).to.be.true;
+                expect(value).to.be.equal(data[key]);
+            }
+        }); 
+    });
+
+    describe('when asked for size', () => {
+        it('should return number of entries', () => {
+            const data: Record<string, any> = { key1: 'value1', key2: 'value2', key3: 'value3' };
+            const { localStorageMap } = setup(data);
+            const size = localStorageMap.size;
+            
+            expect(size).to.be.equal(3);
+        }); 
+    });
+    
+    describe('when asked for string tag', () => {
+        it('should return its name', () => {
+            const { localStorageMap } = setup();
+            const stringTag = localStorageMap[Symbol.toStringTag];
+            
+            expect(stringTag).to.be.equal('LocalStorageMap');
+        }); 
     });
 
     function setup(stored?: any) {
